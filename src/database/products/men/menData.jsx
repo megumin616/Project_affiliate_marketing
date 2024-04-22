38a765f82cf293bc64e39ba3c996a2286347185c
megumin61 <toncom616@gmail.com>
2024-04-22 12:39:36 +0700

    add list product

---
+++ src/database/products/men/menData.jsx
@@ -0,0 +1,17 @@
+//images
+import man1 from '../../../assets/images/men-01.jpg'
+
+export const menData = [
+  { name: "classNameic Spring", price: "$120.00", image: man1 },
+  { name: "Air Force 1 X", price: "$90.00", image: man1 },
+  { name: "Love Nana ‘20", price: "$150.00", image: man1 },
+  { name: "classNameic Spring", price: "$120.00", image: man1 },
+  { name: "Air Force 1 X", price: "$90.00", image: man1 },
+  { name: "Love Nana ‘20", price: "$150.00", image: man1 },
+  { name: "classNameic Spring", price: "$120.00", image: man1 },
+  { name: "Air Force 1 X", price: "$90.00", image: man1 },
+  { name: "Love Nana ‘20", price: "$150.00", image: man1 },
+  { name: "classNameic Spring", price: "$120.00", image: man1 },
+  { name: "Air Force 1 X", price: "$90.00", image: man1 },
+  { name: "Love Nana ‘20", price: "$150.00", image: man1 },
+];
--- src/pages/product/componentsProduct/ProductAll.jsx
+++ src/pages/product/componentsProduct/ProductAll.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import man1 from "../../../assets/images/men-01.jpg";
+import React, { useState } from "react";
+import { menData } from "../../../database/products/men/menData";
 
 //images
 import lazada from "../../../assets/images/LaSh/icons8-lazada.png";
 import shopee from "../../../assets/images/LaSh/icons8-shopee.png";
 
-// ทำให้ข้อมูลเปลี่ยน list ดูสินค้าได้ 
 
-export default function ProductAll() { 
+export default function ProductAll() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // จำนวนรายการต่อหน้า
+
+  const products = menData;
+
+  // คำนวณหน้าทั้งหมด
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  // รายการสินค้าที่ต้องแสดงบนหน้าปัจจุบัน
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, products.length);
+  const currentProducts = products.slice(startIndex, endIndex);
+
+  // เปลี่ยนหน้า
+  const changePage = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
       <section className="section" id="products">
@@ -23,110 +40,56 @@ export default function ProductAll() {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col-lg-4">
-              <div className="item">
-                <div className="thumb">
-                  <div className="hover-content">
-                    <ul>
-                      <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-eye"></i> */}
-                          <img src={lazada} />
-                          <p style={{color: "white"}}>Lazada</p>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-shopping-cart"></i> */}
-                          <img src={shopee} />
-                          <p style={{color: "white"}}>Shopee</p>
-                        </a>
-                      </li>
-                    </ul>
+            {currentProducts.map((product, index) => (
+              <div className="col-lg-4" key={index}>
+                <div className="item">
+                  <div className="thumb">
+                    <div className="hover-content">
+                      <ul>
+                        <li>
+                          <a target="_blank" href="#">
+                            <img src={lazada} />
+                            <p style={{ color: "white" }}>Lazada</p>
+                          </a>
+                        </li>
+                        <li>
+                          <a target="_blank" href="single-product.html">
+                            <img src={shopee} />
+                            <p style={{ color: "white" }}>Shopee</p>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <img src={product.image} alt="" />
                   </div>
-                  <img src={man1} alt="" />
-                </div>
-                <div className="down-content">
-                  <h4>classNameic Spring</h4>
-                  <span>$120.00</span>
+                  <div className="down-content">
+                    <h4>{product.name}</h4>
+                    <span>{product.price}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="item">
-                <div className="thumb">
-                  <div className="hover-content">
-                    <ul>
-                    <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-eye"></i> */}
-                          <img src={lazada} />
-                          <p>Lazada</p>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-shopping-cart"></i> */}
-                          <img src={shopee} />
-                          <p>Shopee</p>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <img src={man1} alt="" />
-                </div>
-                <div className="down-content">
-                  <h4>Air Force 1 X</h4>
-                  <span>$90.00</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="item">
-                <div className="thumb">
-                  <div className="hover-content">
-                    <ul>
-                    <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-eye"></i> */}
-                          <img src={lazada} />
-                          <p>Lazada</p>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="single-product.html">
-                          {/* <i class="fa fa-shopping-cart"></i> */}
-                          <img src={shopee} />
-                          <p>Shopee</p>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <img src={man1} alt="" />
-                </div>
-                <div className="down-content">
-                  <h4>Love Nana ‘20</h4>
-                  <span>$150.00</span>
-                </div>
-              </div>
-            </div>
+            ))}
             <div className="col-lg-12">
               <div className="pagination">
                 <ul>
+                  {[...Array(totalPages)].map((_, index) => (
+                    <li key={index}>
+                      <a
+                        href="#"
+                        onClick={() => changePage(index + 1)}
+                        className={currentPage === index + 1 ? "active" : ""}
+                      >
+                        {index + 1}
+                      </a>
+                    </li>
+                  ))}
                   <li>
-                    <a href="#">1</a>
-                  </li>
-                  <li>
-                    <a href="#">2</a>
-                  </li>
-                  <li>
-                    <a href="#">3</a>
-                  </li>
-                  <li>
-                    <a href="#">4</a>
-                  </li>
-                  <li>
-                    <a href="#"></a>
+                    <a
+                      href="#"
+                      onClick={() => changePage(currentPage === totalPages ? 1 : currentPage + 1)}
+                    >
+                      &gt;
+                    </a>
                   </li>
                 </ul>
               </div>
